591064d09f448fc041f2a2d6a1feb9becbaa0ab6
t-2ddy <theoleonard@live.com>
2025-05-15 16:10:41 -0700

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +7,7 @@ function App() {
     <>
       <div>
         <h1 className='flex text-center justify-center text-4xl '>
-          hello world
+          hello world, this is a test a very long test to see if the text will go off screen
         </h1>
       </div>
     </>
